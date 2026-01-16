41ca8fab0814f00c7fc0826d116d6d49b070a244
long <phan.cheanlong@institute.pse.ngo>
2026-01-16 07:43:30 +0700

    add docker

---
+++ src/test/java/com/booking/booking_api/BookingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booking.booking_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
